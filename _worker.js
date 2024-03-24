ed039fcb5854c619e3749e01864a11b5b8cbe440
ysdw789 <163355317+ysdw789@users.noreply.github.com>
2024-03-24 17:44:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-          url.hostname="14.103.55.75"; // 修改成自己的节点IP/域名
+          url.hostname="www.baidu.com"; // 修改成自己的节点IP/域名
         let new_request = new Request(url, request);
         return fetch(new_request);
       }
